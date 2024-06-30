40b680a6c676f69693d3581813f9bca535b9c8c2
aliraza114 <aliraza3569@gmail.com>
2024-06-30 05:32:38 +0500

    add json data

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
+import { useState } from 'react'
+import { Categories } from './components/Category'
+import { categories } from './data/home'
 import './index.css'
 import {PageHeader} from './layout/PageHeader'
 
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState(categories[0])
 
   return (
     <div className='max-h-screen flex flex-col'>
       <PageHeader></PageHeader>
-      <div>
-        2
-      </div>
+      <div className="grid grid-cols-[auto,1fr] flex-grow-1 overflow-auto">
+          {/* <Sidebar /> */}
+          <div className="overflow-x-hidden px-8 pb-4">
+            <div className="sticky top-0 bg-white z-10 pb-4">
+              <Categories
+                categories={categories}
+                selectedCategory={selectedCategory}
+                onSelect={setSelectedCategory}
+              />
+            </div>
+            <div className="grid gap-4 grid-cols-[repeat(auto-fill,minmax(300px,1fr))]">
+              {/* {videos.map(video => (
+                <VideoGridItem key={video.id} {...video} />
+              ))} */}
+            </div>
+          </div>
+        </div>
     </div>
   )
 }
